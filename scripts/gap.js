5eaa1f3e7aea246d116345660d2eada6d4d2e554
enemiphobia <shainefernandes2008@gmail.com>
2026-08-13 20:51:27 -0400

    fixed jekyll date converter + touchups

--- scripts/gap.js
+++ scripts/gap.js
@@ -8,7 +8,7 @@ const resizeObserver = new ResizeObserver((entries) => {
     for (let entry of entries) {
         const currentWidth = entry.contentRect.width;
 
-        const calculatedGap = Math.max(currentWidth * 0.55, 20);
+        const calculatedGap = Math.max(currentWidth * 0.60, 100);
 
         footerList.style.setProperty('--dynamic-footer-gap', `${calculatedGap}px`);
     }
